16122b47c942064a1e1e5655b5ac7db99f46e9b3
Stephen Molloy <sdmolloy@gmail.com>
2025-09-18 11:40:16 +0200

    Reorg of the build tool

--- build.c
+++ build.c
@@ -15,9 +15,22 @@
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    mkdir_if_not_exists(BINDIR);
-
     Cmd cmd = {0};
+    bool run_exe = false;
+
+    const char* programname = shift_args(&argc, &argv);
+
+    while (argc > 0) {
+        const char *arg = shift_args(&argc, &argv);
+
+        if (strcmp(arg, "run") == 0) run_exe = true;
+        else {
+            nob_log(ERROR, "Argument not recognised: %s", arg);
+            return 1;
+        }
+    }
+
+    mkdir_if_not_exists(BINDIR);
 
     cmd_append(&cmd, "cc");
     cmd_append(&cmd, CFLAGS);
@@ -26,17 +39,9 @@ int main(int argc, char **argv) {
     cmd_append(&cmd, LIBS);
     cmd_run(&cmd);
 
-    const char* programname = shift_args(&argc, &argv);
-
-    if (argc > 0) {
-        const char *arg = shift_args(&argc, &argv);
-        if (strcmp(arg, "run") == 0) {
-            cmd_append(&cmd, "./"BINDIR"/"BINNAME);
-            cmd_run(&cmd);
-        } else {
-            nob_log(ERROR, "Argument not recognised: %s", arg);
-            return 1;
-        }
+    if (run_exe) {
+        cmd_append(&cmd, "./"BINDIR"/"BINNAME);
+        cmd_run(&cmd);
     }
 
     return 0;
